9e70f166c4d1d9fdc06baac6f0e9868a3e3824f6
Patrick Duff <patrick.duff@graham.co.uk>
2020-02-03 13:38:56 +0000

    token finished

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        
+        [Required]
+        [StringLength(8, MinimumLength=4, ErrorMessage = "Your password must be between 4-8 characters")]
+        public string Password { get; set; }
+    }
+}
