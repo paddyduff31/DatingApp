af3cf0d1a4a4b117231aab89749edc069e6c46c2
Patrick Duff <patrick.duff@graham.co.uk>
2020-02-12 14:02:21 +0000

    member edit

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -2,8 +2,12 @@ import {Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-list/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolver/memberdetail.resolver';
+import { MemberListResolver } from './_resolver/memberlist.resolver';
+import { MemberEditComponent } from './members/member-list/member-edit/member-edit.component';
 
 export const appRoutes: Routes = [
     {path: '', component: HomeComponent},
@@ -12,7 +16,11 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            {path: 'members', component: MemberListComponent},
+            {path: 'members', component: MemberListComponent,
+            resolve: {users: MemberListResolver}},
+            {path: 'members/:id', component: MemberDetailComponent,
+            resolve: {user: MemberDetailResolver}},
+            {path: 'member/edit', component: MemberEditComponent},
             {path: 'messages', component: MessagesComponent},
             {path: 'lists', component: ListsComponent}
         ]
